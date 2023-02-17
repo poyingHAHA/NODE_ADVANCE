39d2eaa5f7d88def88f160db1260232896e459bd
poyingHAHA <poying.code@gmail.com>
2023-02-17 17:52:10 +0800

    feat: cluster實作使用cluster.fork()兩次創建兩個監聽server
    要注意在windows要加上"cluster.schedulingPolicy=cluster.SCHED_RR"，
    因為windows好像關閉了Round Robin的功能

--- Enhancing_node_perf/index.js
+++ Enhancing_node_perf/index.js
@@ -2,10 +2,15 @@ const cluster = require('cluster');
 const express = require('express');
 const app = express();
 
+// Round robin process scheduling is disabled in windows
+// enable it by writing " cluster.schedulingPolicy = cluster.SCHED_RR "
+cluster.schedulingPolicy = cluster.SCHED_RR
 if(cluster.isMaster) {
   // Cause index.js to be executed again but in child mode
   cluster.fork()
+  cluster.fork()
 }else{
+
   // child process
   function doWork(duration){
     const staet = Date.now();
@@ -15,6 +20,10 @@ if(cluster.isMaster) {
   app.get('/', (req, res) => {
     doWork(5000);
     res.send('Hi there');
+  })
+
+  app.get('/fast', (req, res) => {
+    res.send("This is fast");
   })
 
   app.listen(3000);
