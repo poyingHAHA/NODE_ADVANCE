67571493679898fab826f9eb31797314d99744f4
poyingHAHA <poying.code@gmail.com>
2023-02-17 17:34:39 +0800

    implement cluster mode by differentiate master and child

--- Enhancing_node_perf/index.js
+++ Enhancing_node_perf/index.js
@@ -2,21 +2,20 @@ const cluster = require('cluster');
 const express = require('express');
 const app = express();
 
-// try to use as much CPU processing power as it possibly can for some set duration.
-// And this duration will be some number of milliseconds.
-function doWork(duration){
-  const staet = Date.now();
-  // while we are running this while loop our event loop can do absolutely nothing else.
-  // That means it cannot handle other requests 
-  // it can't do other like database queries or write files...
-  while(Date.now() - staet < duration){}
+if(cluster.isMaster) {
+  // Cause index.js to be executed again but in child mode
+  cluster.fork()
+}else{
+  // child process
+  function doWork(duration){
+    const staet = Date.now();
+    while(Date.now() - staet < duration){}
+  }
+  
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  })
+
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  // This code right here is going to be executed inside of our event loop
-  // This doesn't get thrown out to an event pool.
-  doWork(5000);
-  res.send('Hi there');
-})
-
-app.listen(3000);
