5d552c41031b719abe46c88b197559c71419d0c5
poyingHAHA <poying.code@gmail.com>
2023-02-17 20:46:37 +0800

    refactor: 去除cluster相關設定，使用pm2做cluster管理
    pm2式管理nodejs process的工具，透過pm2 start -i 0 index.js
    啟動app並最大化使用我們的CPU，可以使用它簡化很多node應用管理的任務
    像是性能監控、自動重啟、loadbalance...。

--- Enhancing_node_perf/index.js
+++ Enhancing_node_perf/index.js
@@ -1,28 +1,17 @@
-const cluster = require('cluster');
+// const cluster = require('cluster');
 const crypto = require('crypto');
 const express = require('express');
 const app = express();
 
-// Round robin process scheduling is disabled in windows
-// enable it by writing " cluster.schedulingPolicy = cluster.SCHED_RR "
-cluster.schedulingPolicy = cluster.SCHED_RR
-if(cluster.isMaster) {
-  // Cause index.js to be executed again but in child mode
-  cluster.fork()
-  cluster.fork()
-  cluster.fork()
-  cluster.fork()
-}else{ // child process
-  
-  app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=> {
-      res.send('Hi there');
-    })
-  })
 
-  app.get('/fast', (req, res) => {
-    res.send("This is fast");
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=> {
+    res.send('Hi there');
   })
+})
 
-  app.listen(3000);
-}
+app.get('/fast', (req, res) => {
+  res.send("This is fast");
+})
+
+app.listen(3000);
